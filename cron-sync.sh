572f16c40d4995164d4e95f1191312905ea147b3
2lach <stefanlachmann@hotmail.com>
2022-11-29 22:44:02 +0100

    added some verifications

--- cron-sync.sh
+++ cron-sync.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/bin/bash
 
 # items to sync and there absolute paths
 USERNAME="pi"
@@ -6,14 +6,26 @@ VIMRC="/home/$USERNAME/.vimrc"
 ZSHENV="/home/pi/.config/zsh/.zshenv"
 ZSHRC="/home/pi/.config/zsh/.zshrc"
 HTOPRC="/home/pi/.config/htop/htoprc"
+REPO="/home/pi/.config/rc-files"
 
 # INIT_NVIM="/Users/$USERNAME/.config/nvim/init.vim"
 # SETTINGS_JSON="/Users/$USERNAME/Library/Application Support/Code/User/settings.json"
-# REPO="/home/pi/.config/rc-files"
-
-# files
-declare -a files=("$VIMRC" "$ZSHENV" "$ZSHRC" "$HTOPRC")
 # "$INIT_VIM" "$SETTINGS_JSON")
+
+function succesful() {
+	# sunc succesful
+	touch /home/pi/rc-files-synced.txt
+	local dt=$(which date).
+	[ -x "$dt" ] && "$dt" >>"/home/pi/rc-files-synced.txt"
+}
+
+# rc files
+declare -a files=(
+	"$VIMRC"
+	"$ZSHENV"
+	"$ZSHRC"
+	"$HTOPRC"
+)
 
 for i in "${files[@]}"; do
 	# verify file path
@@ -23,8 +35,10 @@ for i in "${files[@]}"; do
 	else
 		# sync file(s)
 		echo "sync file: $i"
-		cp "$i" .
+		cp "$i" "$REPO/"
 	fi
+	# verify
+	succesful
 done
 
 # add job to crontab with:
